203a77bbb78feca2a961f4ed9fddd87db4317a37
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-08-05 10:22:12 -0700

    wip

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT: process.env.PORT || 6000, 
+    LADY: process.env.LADY || gaga
+}
--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('web 44 rocks')
+require('dotenv').config() // this allows us to "inject" fake enviorment variables
+
+const { PORT } = require('./config')
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const server = express()
+
+server.use(express.json()) // teaches express to parse req.body
+server.use(cors())
+server.use(helmet())
+
+server.get('/', (req,res)=> {
+    res.send(`
+    <h1>Web 44 is awesome</h1>
+    `)
+})
+
+server.use('*', (req,res)=> {
+    res.json({
+        message: ' WEB 44 is Awesome'
+    })
+})
+server.listen(PORT, ()=> { // heroku uses different port
+    console.log(`listening on ${PORT}`)
+})
+ 
